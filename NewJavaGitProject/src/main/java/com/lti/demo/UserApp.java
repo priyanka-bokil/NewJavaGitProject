ae2aaee422786b81cb5cfd376ca40c00b2261a28
PriyankaBokil <priyankabokil@git.com>
2021-07-28 15:02:35 +0530

    Second Commit

--- NewJavaGitProject/src/main/java/com/lti/demo/UserApp.java
+++ NewJavaGitProject/src/main/java/com/lti/demo/UserApp.java
@@ -3,5 +3,6 @@ package com.lti.demo;
 public class UserApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Check the second commit");
 	}
 }
